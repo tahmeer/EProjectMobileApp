53929c565cc2adb43f37d81c3a1da47e65bd5b6e
Syed Tahmeer Hussain Naqvi <tahmeer.naqvi@meezotech.com>
2022-07-13 17:50:45 -0700

    Ecommerce same

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,61 +3,55 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     //
     public $successStatus = 200;
-    /** 
+/** 
      * login api 
      * 
      * @return \Illuminate\Http\Response 
-     */
-    public function login()
-    {
-        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
-            $user = Auth::user();
-            $success['token'] =  $user->createToken('MyApp')->accessToken;
-            return response()->json(['success' => $success], $this->successStatus);
-        } else {
-            return response()->json(['error' => 'Unauthorised'], 401);
-        }
+     */ 
+    public function login(){ 
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
+            $user = Auth::user(); 
+            $success['token'] =  $user->createToken('MyApp')-> accessToken; 
+            return response()->json(['success' => $success], $this-> successStatus); 
+        } 
+        else{ 
+            return response()->json(['error'=>'Unauthorised'], 401); 
+        } 
     }
-    /** 
+/** 
      * Register api 
      * 
      * @return \Illuminate\Http\Response 
-     */
-    public function register(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'email' => 'required|email|unique:users',
-            'password' => 'required',
-            // 'image'=>'required',
+     */ 
+    public function register(Request $request) 
+    { 
+        $validator = Validator::make($request->all(), [ 
+            'name' => 'required', 
+            'email' => 'required|email|unique:users', 
+            'password' => 'required', 
             // 'c_password' => 'required|same:password', 
         ]);
-        if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors()], 401);
+if ($validator->fails()) { 
+            return response()->json(['error'=>$validator->errors()], 401);            
         }
-        // $user = new User();
-        // $user->name = $request->name;
-        // $user->email = $request->email;
-        // $user->password = bcrypt($request->password);
+
         // $path = 'public\User\images';
         // $image = $request->image->store($path);
         // $user->image = str_replace('public/', '', $image);
-        // $user->save();
-       
-        
-        $input = $request->all();
-        $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        $success['token'] =  $user->createToken('MyApp')->accessToken;
+$input = $request->all(); 
+        $input['password'] = bcrypt($input['password']); 
+        $user = User::create($input); 
+        $success['token'] =  $user->createToken('MyApp')-> accessToken; 
         $success['name'] =  $user->name;
-        return response()->json(['success' => $success], $this->successStatus);
+return response()->json(['success'=>$success], $this-> successStatus); 
     }
+
 }
